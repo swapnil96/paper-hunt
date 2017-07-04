fc5aa0df1c1bf34aecc1e69ea2d16b3dbf20569e
swapnil <dasswapnil96@gmail.com>
2017-07-05 01:31:30 +0800

    Setup file modified

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+import os
 from setuptools import setup
+
+setup(name='paper-hunt',
+      version='0.1',
+      description='Module for data extraction',
+      url='https://github.com/swapnil96/paper-hunt',
+      author='Swapnil Das',
+      author_email='dassswapnil96@gmail.com',
+      license='GNU General Public License v3.0',
+      packages=['paper-hunt'],
+      install_requires=[
+          'selenium',
+      ],
+      zip_safe=False)
