c46043cae6d130ca664c1d6d24a2ce6700017658
swapnil <dasswapnil96@gmail.com>
2017-07-06 23:51:32 +0800

    Updated code for query

--- main.py
+++ main.py
@@ -11,24 +11,57 @@
 # See for which papers it is mentioned in related work section
 
 # Download files that cited it.
-from Download import download
-from Process import *
-download.download(name)
+# from Download import download
+from Section import related_work
+from Section import reference
+import re
+import os 
 
-# Process all the PDFs to text and find the related work section in all of them.
-process.process_dir(name)
-
-related_work.process_dir(name)
-
+# download.download(name)
+name = "FICA"
 # Logic for checking if a PDF is mentioned in other in related work section.
-refs = os.listdir("Repo/related_work/" + name)
-for ref in refs:
+papers = os.listdir("Repo/" + name)
+for paper in papers:
+    # Get the necessary sections
+    rw_sec = related_work.find(paper)
+    ref_sec = reference.find(paper)
+    lis = re.findall('\[[^\]]*\]', sec)
+    if lis:
+        # Found related_work section
+        temp = lis.split(',')
+        num = []
+        for t in xrange(len(temp)):
+            if t == 0:
+                tt = temp[t].split('[')
+                if '-' in tt[1]:
+                    ttt = tt[1].split('-')
+                    num.append(int(ttt[0]))
+                    num.append(int(ttt[1]))
 
-    for line in ref:
-        # 1st check with direct name
-        if ref in line:
-            
-        # Second name with numbering
-        else:
+                else:
+                    num.append(int(tt[1]))
 
-#  DO processing
+            elif t == len(temp) - 1:
+                tt = temp[t].split(']')
+                if '-' in tt[1]:
+                    ttt = tt[1].split('-')
+                    num.append(int(ttt[0]))
+                    num.append(int(ttt[1]))
+
+                else:
+                    num.append(int(tt[1]))
+
+            else:
+                if '-' in temp[t]:
+                    ttt = temp[t].split('-')
+                    num.append(int(ttt[0]))
+                    num.append(int(ttt[1]))
+
+                else:
+                    num.append(int(temp[t]))
+
+        for n in num:
+            ref = ref_sec[n]
+            if name in ref:
+                ans.append(paper)
+        
